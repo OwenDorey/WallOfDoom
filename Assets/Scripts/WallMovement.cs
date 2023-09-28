3ad38071591854efa7674f43b9a56316641f9c8e
OwenDorey <123426282+OwenDorey@users.noreply.github.com>
2023-09-28 11:23:28 +0100

    v4

--- Assets/Scripts/WallMovement.cs
+++ Assets/Scripts/WallMovement.cs
@@ -9,13 +9,14 @@ public class WallMovement : MonoBehaviour
     public float cameraFollowSpeed = 2f;
     public float wallMoveSpeed = 1f;
 
-    private float offset = 8.4f;
+    private float offset = 9f;
     void Update()
     {
         Vector3 wallPosition = new Vector3((wall.position.x + 1), wall.position.y, 0);
         wall.position = Vector3.Lerp(wall.position, wallPosition, wallMoveSpeed*Time.deltaTime);
 
         Vector3 camPosition = new Vector3((wall.position.x + offset), wall.position.y, -10f);
-        cam.position = Vector3.Lerp(cam.position, camPosition, cameraFollowSpeed*Time.deltaTime);
+        //cam.position = Vector3.Lerp(cam.position, camPosition, cameraFollowSpeed*Time.deltaTime);
+        cam.position = camPosition;
     }
 }
