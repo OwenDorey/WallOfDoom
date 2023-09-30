c25cbd9d36ef4e0d5b82c9ee28698b1246c259ff
OwenDorey <16odorey@gmail.com>
2023-10-01 00:34:33 +0100

    v9

--- Assets/GameScripts/LevelGeneration.cs
+++ Assets/GameScripts/LevelGeneration.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class LevelGeneration : MonoBehaviour
 {
     public static GameObject[] levels;
+
     public Transform cam;
     public GameObject environment;
+    public GameObject powerUp;
 
     private int levelPosition = 0;
     
@@ -27,8 +29,15 @@ public class LevelGeneration : MonoBehaviour
     void GenerateLevel()
     {
         int randomLevel = Random.Range(0, levels.Length);
-        GameObject level = Instantiate(levels[randomLevel]) as GameObject;
+
+        GameObject level = Instantiate(levels[randomLevel], environment.transform) as GameObject;
         level.transform.position = new Vector3(levelPosition, 0, 0);
-        level.transform.parent = environment.transform;
+
+
+        int powerUpX = Random.Range(-9,9);
+        int powerUpY = Random.Range(-5,5);
+
+        GameObject newPowerUp = Instantiate(powerUp, level.transform);
+        newPowerUp.transform.position = new Vector3((powerUpX + levelPosition), powerUpY, 0);
     }
 }
